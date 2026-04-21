446f986e91d44ffec902d34d90f9aeb79e833372
Penrose480 <Penrose480@proton.me>
2026-04-20 19:53:29 -0700

    added saving fucntion

--- editx.c
+++ editx.c
@@ -7,6 +7,7 @@
 #include <asm-generic/errno-base.h>
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -283,6 +284,19 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+}
+
+void editorSave() {
+  if (E.filename == NULL) return;
+
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  ftruncate(fd, len);
+  write(fd, buf, len);
+  close(fd);
+  free(buf);
 }
 
 /*** append buffer **/
