adec461cec5fbd05f9fedd3fad1904c02d2d393b
Penrose480 <Penrose480@proton.me>
2026-07-05 13:05:01 -0700

    fixed typo in editor name

--- editx.c
+++ editx.c
@@ -750,7 +750,7 @@ void editorDrawRows(struct abuf *ab) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
-          "EDITX editor -- version %s", EDITX_VERSION);
+          "editx -- version %s", EDITX_VERSION);
         if (welcomelen > E.screencols) welcomelen = E.screencols;
         int padding = (E.screencols - welcomelen) / 2;
         if (padding) {
